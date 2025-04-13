c001b16954b505421da1eb8ff079840e1604c1b5
Aashish518 <jadavaashish9@gmail.com.com>
2025-04-13 14:12:44 +0530

    coockis

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 
 app.use(cors({
-    origin: true, 
+    origin: "https://findbooksfront.onrender.com", 
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
