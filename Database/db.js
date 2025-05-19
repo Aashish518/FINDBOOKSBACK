54933c015b02b5e27703cd65e5865f04c95fa305
Aashish518 <jadavaashish9@gmail.com.com>
2025-05-19 09:40:15 +0530

    cc

--- Database/db.js
+++ Database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToMongo = () => {
-mongoose.connect("mongodb://localhost:27017/FINDBOOKS",{
+    mongoose.connect("mongodb+srv://jadavaashish9:An%40211518@cluster0.gtxxzpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
